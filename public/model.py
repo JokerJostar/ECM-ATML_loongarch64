daef47cd61e026146bdca35375dcc694a9996ab9
leader <2530204503@qq.com>
2024-06-08 21:23:50 +0800

    jo

--- public/model.py
+++ public/model.py
@@ -23,53 +23,64 @@ class AFNet(nn.Module):
     def __init__(self):
         super(AFNet, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=3, kernel_size=(6, 1), stride=(2,1), padding=0),
+            nn.Conv2d(in_channels=1, out_channels=3, kernel_size=(6, 1), stride=(2, 1), padding=0),
             nn.ReLU(True),
             nn.BatchNorm2d(3, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=5, kernel_size=(5, 1), stride=(2,1), padding=0),
+            nn.Conv2d(in_channels=3, out_channels=5, kernel_size=(5, 1), stride=(2, 1), padding=0),
             nn.ReLU(True),
             nn.BatchNorm2d(5, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
         self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=5, out_channels=10, kernel_size=(4, 1), stride=(2,1), padding=0),
+            nn.Conv2d(in_channels=5, out_channels=10, kernel_size=(4, 1), stride=(2, 1), padding=0),
             nn.ReLU(True),
             nn.BatchNorm2d(10, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
         self.conv4 = nn.Sequential(
-            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(4, 1), stride=(2,1), padding=0),
+            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(4, 1), stride=(2, 1), padding=0),
             nn.ReLU(True),
             nn.BatchNorm2d(20, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
         self.conv5 = nn.Sequential(
-            nn.Conv2d(in_channels=20, out_channels=20, kernel_size=(4, 1), stride=(2,1), padding=0),
+            nn.Conv2d(in_channels=20, out_channels=20, kernel_size=(4, 1), stride=(2, 1), padding=0),
             nn.ReLU(True),
             nn.BatchNorm2d(20, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
+        # 计算卷积层后的输出形状
+        self.flatten_size = self._get_conv_output((1, 1250, 1))
+
         self.fc1 = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(in_features=740, out_features=10)
+            nn.Linear(in_features=self.flatten_size, out_features=10)
         )
         self.fc2 = nn.Sequential(
             nn.Linear(in_features=10, out_features=2)
         )
 
+    def _get_conv_output(self, shape):
+        # 用一个虚拟输入通过所有卷积层以计算输出形状
+        input = torch.rand(1, *shape)
+        output = self._forward_conv(input)
+        return int(torch.prod(torch.tensor(output.size())))
+
+    def _forward_conv(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        return x
+
     def forward(self, input):
-
-        conv1_output = self.conv1(input)
-        conv2_output = self.conv2(conv1_output)
-        conv3_output = self.conv3(conv2_output)
-        conv4_output = self.conv4(conv3_output)
-        conv5_output = self.conv5(conv4_output)
-        conv5_output = conv5_output.view(-1,740)
-
-        fc1_output = F.relu(self.fc1(conv5_output))
+        conv_output = self._forward_conv(input)
+        conv_output = conv_output.view(conv_output.size(0), -1)  # Flatten the tensor
+        fc1_output = F.relu(self.fc1(conv_output))
         fc2_output = self.fc2(fc1_output)
         return fc2_output
 
