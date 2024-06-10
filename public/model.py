e2f49cf0794b8a6aa2af95921a52ca892239729b
inspur <2530204503@qq.com>
2024-06-10 18:22:25 +0800

    final SiLU

--- public/model.py
+++ public/model.py
@@ -33,7 +33,7 @@ class SqueezeExciteAF(nn.Module):
     def forward(self, x):
         scale = F.adaptive_avg_pool2d(x, 1)
         scale = self.fc1(scale)
-        scale = F.relu(scale, inplace=True)
+        scale = F.silu(scale)  # 使用SiLU激活函数
         scale = self.fc2(scale)
         scale = torch.sigmoid(scale)
         return x * scale
@@ -75,7 +75,8 @@ class AFNet(nn.Module):
 
         self.fc1 = nn.Sequential(
             nn.Dropout(0.5),  # 使用适度的Dropout
-            nn.Linear(in_features=1184, out_features=10)
+            nn.Linear(in_features=1184, out_features=10),
+            nn.SiLU(inplace=True)  # 使用SiLU激活函数
         )
         self.fc2 = nn.Sequential(
             nn.Linear(in_features=10, out_features=2)
@@ -89,7 +90,7 @@ class AFNet(nn.Module):
         conv5_output = self.conv5(conv4_output)
         conv5_output = conv5_output.view(conv5_output.size(0), -1)
 
-        fc1_output = F.leaky_relu(self.fc1(conv5_output), negative_slope=0.1)
+        fc1_output = self.fc1(conv5_output)
         fc2_output = self.fc2(fc1_output)
         return fc2_output
 
