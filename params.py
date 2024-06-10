7c2bcccf3ae92ec1be7258e000913a4141e34e62
inspur <2530204503@qq.com>
2024-06-10 17:52:21 +0800

    final LeakyReLU

--- params.py
+++ params.py
@@ -14,7 +14,7 @@ MODEL_SAVE_PATH = 'temp/saved_model/saved'
 INPUT_SIZE = 1250
 
 # The number of epochs for training the model
-NUM_EPOCHS = 20
+NUM_EPOCHS = 30
 
 # The size of the batches used in training and validation
 BATCH_SIZE = 32
--- public/model.py
+++ public/model.py
@@ -23,7 +23,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class SqueezeExciteAF(nn.Module):
     def __init__(self, in_channels, reduction=4):
         super(SqueezeExciteAF, self).__init__()
@@ -34,7 +33,7 @@ class SqueezeExciteAF(nn.Module):
     def forward(self, x):
         scale = F.adaptive_avg_pool2d(x, 1)
         scale = self.fc1(scale)
-        scale = F.leaky_relu(scale, negative_slope=0.1, inplace=True)
+        scale = F.relu(scale, inplace=True)
         scale = self.fc2(scale)
         scale = torch.sigmoid(scale)
         return x * scale
@@ -44,40 +43,39 @@ class AFNet(nn.Module):
         super(AFNet, self).__init__()
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=4, kernel_size=(6, 1), stride=(2,1), padding=0),
-            nn.LeakyReLU(negative_slope=0.1, inplace=True),
+            nn.SiLU(inplace=True),
             nn.BatchNorm2d(4, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
         self.conv2 = nn.Sequential(
             nn.Conv2d(in_channels=4, out_channels=8, kernel_size=(5, 1), stride=(2,1), padding=0),
-            nn.LeakyReLU(negative_slope=0.1, inplace=True),
+            nn.SiLU(inplace=True),
             nn.BatchNorm2d(8, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
         self.conv3 = nn.Sequential(
             nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(4, 1), stride=(2,1), padding=0),
-            nn.LeakyReLU(negative_slope=0.1, inplace=True),
+            nn.SiLU(inplace=True),
             nn.BatchNorm2d(16, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
             SqueezeExciteAF(16)
         )
 
         self.conv4 = nn.Sequential(
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(4, 1), stride=(2,1), padding=0),
-            nn.LeakyReLU(negative_slope=0.1, inplace=True),
+            nn.SiLU(inplace=True),
             nn.BatchNorm2d(32, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
         self.conv5 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1), stride=(2,1), padding=0),
-            nn.LeakyReLU(negative_slope=0.1, inplace=True),
+            nn.SiLU(inplace=True),
             nn.BatchNorm2d(32, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
             SqueezeExciteAF(32)
         )
 
         self.fc1 = nn.Sequential(
             nn.Dropout(0.5),  # 使用适度的Dropout
-            nn.Linear(in_features=1184, out_features=10),
-            nn.LeakyReLU(negative_slope=0.1, inplace=True)  # 使用LeakyReLU激活函数
+            nn.Linear(in_features=1184, out_features=10)
         )
         self.fc2 = nn.Sequential(
             nn.Linear(in_features=10, out_features=2)
@@ -91,7 +89,7 @@ class AFNet(nn.Module):
         conv5_output = self.conv5(conv4_output)
         conv5_output = conv5_output.view(conv5_output.size(0), -1)
 
-        fc1_output = self.fc1(conv5_output)
+        fc1_output = F.leaky_relu(self.fc1(conv5_output), negative_slope=0.1)
         fc2_output = self.fc2(fc1_output)
         return fc2_output
 
