fed0787963e8adaa7aca488d8d86181470c267c8
inspur <2530204503@qq.com>
2024-06-10 16:44:53 +0800

    jo

--- public/model.py
+++ public/model.py
@@ -23,6 +23,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class SqueezeExciteAF(nn.Module):
     def __init__(self, in_channels, reduction=4):
         super(SqueezeExciteAF, self).__init__()
@@ -42,40 +43,40 @@ class AFNet(nn.Module):
     def __init__(self):
         super(AFNet, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=3, kernel_size=(6, 1), stride=(2,1), padding=0),
-            nn.SiLU(True),
-            nn.BatchNorm2d(3, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
+            nn.Conv2d(in_channels=1, out_channels=4, kernel_size=(6, 1), stride=(2,1), padding=0),
+            nn.SiLU(inplace=True),
+            nn.BatchNorm2d(4, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=5, kernel_size=(5, 1), stride=(2,1), padding=0),
-            nn.SiLU(True),
-            nn.BatchNorm2d(5, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
+            nn.Conv2d(in_channels=4, out_channels=8, kernel_size=(5, 1), stride=(2,1), padding=0),
+            nn.SiLU(inplace=True),
+            nn.BatchNorm2d(8, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
         self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=5, out_channels=10, kernel_size=(4, 1), stride=(2,1), padding=0),
-            nn.SiLU(True),
-            nn.BatchNorm2d(10, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
-            SqueezeExciteAF(10)
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(4, 1), stride=(2,1), padding=0),
+            nn.SiLU(inplace=True),
+            nn.BatchNorm2d(16, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
+            SqueezeExciteAF(16)
         )
 
         self.conv4 = nn.Sequential(
-            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(4, 1), stride=(2,1), padding=0),
-            nn.SiLU(True),
-            nn.BatchNorm2d(20, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(4, 1), stride=(2,1), padding=0),
+            nn.SiLU(inplace=True),
+            nn.BatchNorm2d(32, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
         )
 
         self.conv5 = nn.Sequential(
-            nn.Conv2d(in_channels=20, out_channels=20, kernel_size=(4, 1), stride=(2,1), padding=0),
-            nn.SiLU(True),
-            nn.BatchNorm2d(20, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
-            SqueezeExciteAF(20)
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1), stride=(2,1), padding=0),
+            nn.SiLU(inplace=True),
+            nn.BatchNorm2d(32, affine=True, track_running_stats=True, eps=1e-5, momentum=0.1),
+            SqueezeExciteAF(32)
         )
 
         self.fc1 = nn.Sequential(
-            nn.Dropout(0.6),  # 增强Dropout
-            nn.Linear(in_features=740, out_features=10)
+            nn.Dropout(0.5),  # 使用适度的Dropout
+            nn.Linear(in_features=1184, out_features=10)
         )
         self.fc2 = nn.Sequential(
             nn.Linear(in_features=10, out_features=2)
@@ -87,12 +88,11 @@ class AFNet(nn.Module):
         conv3_output = self.conv3(conv2_output)
         conv4_output = self.conv4(conv3_output)
         conv5_output = self.conv5(conv4_output)
-        conv5_output = conv5_output.view(-1, 740)
+        conv5_output = conv5_output.view(conv5_output.size(0), -1)
 
-        fc1_output = F.silu(self.fc1(conv5_output))
+        fc1_output = F.leaky_relu(self.fc1(conv5_output), negative_slope=0.1)
         fc2_output = self.fc2(fc1_output)
         return fc2_output
-
 class DepthwiseSeparableConv(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super(DepthwiseSeparableConv, self).__init__()
